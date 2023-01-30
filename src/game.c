6462f7e4720d62ae5d6f5be716c15711b165ee6d
Nathan Tirolf <nathan.tirolf@epitech.eu>
2023-01-30 09:35:27 +0100

    [MOD] Rename struct

--- src/game.c
+++ src/game.c
@@ -9,16 +9,14 @@
 
 void game_loop(all_info *all)
 {
-    s_i2 *move = malloc(sizeof(s_i2));
-    move->x = 1; move->y = 1;
+    s_i2 *t_move = malloc(sizeof(s_i2)); t_move->x = 1; t_move->y = 1;
     while (verif_end(all) == 0){
-        choose_placement(move, all);
-        refresh(); clear();
+        choose_placement(t_move, all); refresh(); clear();
         if ( verif_end(all))
             break;
         ai_turn(all);
     }
-    free(move); refresh(); clear();
+    free(t_move); refresh(); clear();
     printw("\n");
     for (int i = 0; i < 3 ; i++)
         printw("%c|%c|%c\n", B[i][0], B[i][1], B[i][2]);
--- src/player.c
+++ src/player.c
@@ -7,38 +7,37 @@
 
 #include "../include/tic_tac_toe.h"
 
-s_i2 *set_movement(s_i2 *move, int key)
+s_i2 *set_t_movement(s_i2 *t_move, int key)
 {
-    if ( key == KEY_UP && move->x > 0)
-        move->x--;
-    if ( key == KEY_DOWN && move->x < 2)
-        move->x++;
-    if ( key == KEY_LEFT && move->y > 0)
-        move->y--;
-    if ( key == KEY_RIGHT && move->y < 2)
-        move->y++;
-    return move;
+    if ( key == KEY_UP && t_move->x > 0)
+        t_move->x--;
+    if ( key == KEY_DOWN && t_move->x < 2)
+        t_move->x++;
+    if ( key == KEY_LEFT && t_move->y > 0)
+        t_move->y--;
+    if ( key == KEY_RIGHT && t_move->y < 2)
+        t_move->y++;
+    return t_move;
 }
 
-void choose_placement(s_i2 *move, all_info *all)
+void choose_placement(s_i2 *t_move, all_info *all)
 {
     char **dup = duptab(B); int key = 0;
     while (1){
         printw("\n");
         for (int i = 0; i < 3 ; i++)
             printw("%c|%c|%c\n", dup[i][0], dup[i][1], dup[i][2]);
-        dup[move->x][move->y] = B[move->x][move->y]; key = getch();
+        dup[t_move->x][t_move->y] = B[t_move->x][t_move->y]; key = getch();
         if (key == 10)
             break;
-        move = set_movement(move, key); dup[move->x][move->y] = '+';
+        t_move = set_t_movement(t_move, key); dup[t_move->x][t_move->y] = '+';
         refresh(); clear();
     }
-    if ( B[move->x][move->y] != '.'){
+    if ( B[t_move->x][t_move->y] != '.'){
         printw("\nInvalid Position\n"); refresh();
-        usleep(1000000); clear();
-        choose_placement(move, all);
+        usleep(1000000); clear(); choose_placement(t_move, all);
     } else {
-        B[move->x][move->y] = 'X';
+        B[t_move->x][t_move->y] = 'X';
     }
     free(dup);
 }
